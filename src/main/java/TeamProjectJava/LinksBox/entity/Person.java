e22b428ce6fdc50add99e4fedc975530c1d789e9
Alexander Macakov <macakovaleksandr@gmail.com>
2019-01-16 15:32:31 +0400

    edit generation type for id

--- src/main/java/TeamProjectJava/LinksBox/entity/Person.java
+++ src/main/java/TeamProjectJava/LinksBox/entity/Person.java
@@ -15,19 +15,28 @@ import java.util.LinkedHashSet;
 public class Person {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
     @NotBlank(message = "firstName cannot be empty")
+    @Column (name = "first_name", nullable = false)
     private String firstName;
+
     @NotBlank(message = "lastName cannot be empty")
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+
     @Email
     @NotBlank(message = "email cannot be empty")
+    @Column(name = "email", nullable = false)
     private String email;
+
     @NotBlank(message = "password cannot be empty")
+    @Column(name = "password", nullable = false)
     private String password;
 
-    @OneToMany(mappedBy = "catalog", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "catalog", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Column(name = "catalog_id", nullable = false)
     private LinkedHashSet<Catalog> catalogs;
 
     public Person(String firstName, String lastName, @Email String email, String password, LinkedHashSet<Catalog> catalogs) {
