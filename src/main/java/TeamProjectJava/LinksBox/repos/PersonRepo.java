068784b0a0c8c69fc04f6a11a66761f7862a6486
hamster <ask@crazy.org.ua>
2019-02-01 14:58:57 +0200

    add PersonRepo

---
+++ src/main/java/TeamProjectJava/LinksBox/repos/PersonRepo.java
@@ -0,0 +1,7 @@
+package TeamProjectJava.LinksBox.repos;
+
+import TeamProjectJava.LinksBox.entity.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepo extends CrudRepository<Person, Long>{
+}
